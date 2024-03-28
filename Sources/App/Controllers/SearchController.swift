41ec2640ed8bd275901011d7cb31d2fcbfc0e182
Salva Moreno <morenosanchezsalva@gmail.com>
2024-03-28 21:21:31 +0100

    Get good response when searching for messages

--- Sources/App/Controllers/SearchController.swift
+++ Sources/App/Controllers/SearchController.swift
@@ -15,7 +15,7 @@ struct SearchController: RouteCollection {
 }
 
 extension SearchController {
-    func search(req: Request) async throws -> String {
+    func search(req: Request) async throws -> [Message] {
         guard let searchText: String = req.query["search"] else {
             throw Abort(.badRequest)
         }
@@ -24,7 +24,11 @@ extension SearchController {
         
         let smartSearchMatcher = SmartSearchMatcher(searchString: spaceStrippedSearchText)
         
-        return ""
+        let allMessages: [Message] = try await Message.query(on: req.db).all()
+        
+        let matchedMessages: [Message] = allMessages.filter { smartSearchMatcher.matches($0.message) }
+        
+        return matchedMessages
     }
 }
 
@@ -34,7 +38,24 @@ struct SmartSearchMatcher {
     }
     
     func matches(_ candidateString: String) -> Bool {
-        // TODO
+        guard !searchTokens.isEmpty else { return true }
+        
+        var candidateStringTokens = candidateString.split(whereSeparator: { $0.isWhitespace })
+        
+        for searchToken in searchTokens {
+            var matchedSearchToken = false
+            
+            for (candidateStringTokenIndex, candidateStringToken) in candidateStringTokens.enumerated() {
+                if let range = candidateStringToken.range(of: searchToken, options: [.caseInsensitive, .diacriticInsensitive]),
+                   range.lowerBound == candidateStringToken.startIndex {
+                    matchedSearchToken = true
+                    candidateStringTokens.remove(at: candidateStringTokenIndex)
+                    break
+                }
+            }
+            
+            guard matchedSearchToken else { return false }
+        }
         
         return true
     }
