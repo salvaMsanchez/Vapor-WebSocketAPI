706e856f9c6a0f363994a01986fe3c2b63357167
Salva Moreno <morenosanchezsalva@gmail.com>
2024-03-30 18:19:27 +0100

    Configure migration for adding User schema on db

--- Sources/App/Migrations/ModelsMigration_v0.swift
+++ Sources/App/Migrations/ModelsMigration_v0.swift
@@ -11,6 +11,16 @@ import Fluent
 struct ModelsMigration_v0: AsyncMigration {
     func prepare(on database: Database) async throws {
         try await database
+            .schema(User.schema)
+            .id()
+            .field("created_at", .string)
+            .field("name", .string, .required)
+            .field("email", .string, .required)
+            .field("password", .string, .required)
+            .unique(on: "email")
+            .create()
+        
+        try await database
             .schema(Message.schema)
             .id()
             .field("username", .string, .required)
@@ -21,6 +31,7 @@ struct ModelsMigration_v0: AsyncMigration {
     }
     
     func revert(on database: Database) async throws {
+        try await database.schema(User.schema).delete()
         try await database.schema(Message.schema).delete()
     }
 }
